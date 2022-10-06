5eb4fb6d7adc023e2dbe9f46f161adfa444acd0f
jsemalja <jsemaljaa@gmail.com>
2022-10-06 16:13:50 +0200

    files structure changes

---
+++ src/scanner.c
@@ -0,0 +1,5 @@
+//
+// Created by jsema on 10/6/2022.
+//
+
+#include "scanner.h"
---
+++ src/scanner.h
@@ -0,0 +1,8 @@
+//
+// Created by jsema on 10/6/2022.
+//
+
+#ifndef PROJECT_SCANNER_H
+#define PROJECT_SCANNER_H
+
+#endif //PROJECT_SCANNER_H
