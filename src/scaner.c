ff350736e1b4518a69c0c4c46c01c35c7ac3d9ab
evgenia-taipova <evgenia.taipova@yandex.ru>
2022-10-18 21:32:27 +0200

    jgjjg

---
+++ src/scaner.c
@@ -0,0 +1,475 @@
+// lexikalni analyzator
+#include <stdio.h>
+#include <ctype.h>
+#include "str.h"
+#include "scaner.h"
+
+// promenna pro ulozeni vstupniho souboru
+FILE *source;
+
+void setSourceFile(FILE *f)
+{
+    source = f;
+}
+
+int getNextToken(string *attr)
+// hlavni funkce lexikalniho analyzatoru
+{
+    int state = 0;
+    int c;
+    // vymazeme obsah atributu a v pripade identifikatoru
+    // budeme postupne do nej vkladat jeho nazev
+    strClear(attr);
+
+    while (1)
+    {
+        // nacteni dalsiho znaku
+        c = getc(source);
+
+        switch (state)
+        {
+        case STATE_START:
+            // zakladni stav automatu
+            if (isspace(c))
+                state = STATE_START;
+
+            else if (c == '+')
+                return PLUS;
+
+            else if (c == '-')
+                return MINUS;
+
+            else if (c == '*')
+                return MUL;
+
+            else if (c == '-')
+                return MINUS;
+
+            else if (c == '(')
+                return LEFT_PAR;
+
+            else if (c == ')')
+                return RIGHT_PAR;
+
+            else if (c == ':')
+                return COLON;
+
+            else if (c == '{')
+                return LEFT_BR;
+
+            else if (c == '}')
+                return RIGHT_BR;
+
+            else if (c == ';')
+                return SEMICOLON;
+
+            else if (c == EOF)
+                return END_OF_FILE;
+
+            else if (c == '\n')
+                return END_OF_LINE;
+
+            else if (c == '.')
+                return POINT;
+
+            else if (c == ',')
+                return COMMA;
+
+            else if (c == '<')
+                state = STATE_LESS_THAN;
+
+            else if (c == '>')
+                state = STATE_MORE_THAN;
+
+            else if (c == '=')
+                state = STATE_EQUAL_START;
+
+            else if (c == '!')
+                state = STATE_NOT_EQUAL_START;
+
+            else if (c == '"')
+                state = STATE_STRING_START;
+
+            else if (c == '/')
+                state = STATE_DIV;
+
+            else if (isdigit(c))
+            {
+                strAddChar(attr, c);
+                state = STATE_NUMBER;
+            }
+
+            else if (isalpha(c) || c == '_')
+            {
+                strAddChar(attr, c);
+                state = STATE_IDENTIFIER_OR_KEYWORD;
+            }
+
+            else
+                return LEX_ERROR;
+
+            break;
+
+        case STATE_LESS_THAN: // <
+
+            if (c == '=')
+                return LESS_EQ; // <=
+
+            else if (c == EOF)
+                return LEX_ERROR;
+
+            ungetc(c, source);
+            return LESS;
+
+            break;
+
+        case STATE_MORE_THAN: // >
+            // komentar
+            if (c == '=')
+                return GREATER_EQ; // >=
+
+            else if (c == EOF)
+                return LEX_ERROR;
+
+            ungetc(c, source);
+            return GREATER;
+            break;
+
+        case STATE_EQUAL_START: // =
+            if (c == '=')
+                state = STATE_EQUAL; // ==
+            else
+            {
+                if (c == EOF)
+                    return LEX_ERROR;
+                ungetc(c, source);
+                return ASSIGN;
+            }
+            break;
+
+        case STATE_NOT_EQUAL_START: // !
+            if (c == '=')
+                state = STATE_NOT_EQUAL; // !=
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_EQUAL: // ==
+            if (c == '=')
+                return EQUAL; // ===
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_NOT_EQUAL: // !=
+            if (c == '=')
+                return NOT_EQUAL; // !==
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_DIV: // /
+            // komentar
+            if (c == '/')
+                state = STATE_COMMENTARY; // //
+
+            else if (c == '*')
+                state = STATE_BLOCK_COMMENTARY; // /*
+
+            else if (c == EOF)
+                return LEX_ERROR;
+
+            ungetc(c, source);
+            return DIV;
+            break;
+
+        case STATE_COMMENTARY: // //
+            if (c == '\n' || c == EOF)
+            {
+                state = STATE_START;
+                ungetc(c, source);
+            }
+            break;
+
+        case STATE_BLOCK_COMMENTARY: // /*
+            if (c == '*')
+                state = STATE_BLOCK_COMMENTARY_LEAVE; // /**
+            else if (c == EOF)
+                return LEX_ERROR;
+            break;
+
+        case STATE_BLOCK_COMMENTARY_LEAVE: // /**
+            if (c == '/')
+                state = STATE_START; // /**/
+            else if (c == EOF)
+                return LEX_ERROR;
+            else
+                state = STATE_BLOCK_COMMENTARY;
+            break;
+
+        case STATE_IDENTIFIER_OR_KEYWORD:
+            // identifikator nebo klicove slovo
+            if (isalpha(c) || c == '_')
+                // identifikator pokracuje
+                strAddChar(attr, c);
+            else
+            // konec identifikatoru
+            {
+                ungetc(c, source); // POZOR! Je potreba vratit posledni nacteny znak
+
+                // kontrola, zda se nejedna o klicove slovo
+                if (strCmpConstStr(attr, "else") == 0)
+                    return ELSE;
+
+                else if (strCmpConstStr(attr, "float") == 0)
+                    return FLOAT;
+
+                else if (strCmpConstStr(attr, "function") == 0)
+                    return FUNCTION;
+
+                else if (strCmpConstStr(attr, "if") == 0)
+                    return IF;
+
+                else if (strCmpConstStr(attr, "int") == 0)
+                    return INT;
+
+                // else if (strCmpConstStr(attr, "null") == 0)
+                //   return NULL;
+
+                else if (strCmpConstStr(attr, "return") == 0)
+                    return RETURN;
+
+                else if (strCmpConstStr(attr, "string") == 0)
+                    return STRING;
+
+                else if (strCmpConstStr(attr, "void") == 0)
+                    return VOID;
+
+                else if (strCmpConstStr(attr, "while") == 0)
+                    return WHILE;
+
+                // jednalo se skutecne o identifikator
+                return ID;
+            }
+            break;
+
+        case STATE_NUMBER:
+            if (c == 'e' || c == 'E')
+            {
+                state = STATE_NUMBER_EXPONENT;
+                strAddChar(attr, c);
+            }
+            else if (isdigit(c))
+                strAddChar(attr, c);
+            else if (c == '.')
+            {
+                state = STATE_NUMBER_POINT;
+                strAddChar(attr, c);
+            }
+            else
+            {
+                ungetc(c, source);
+                return TYPE_INT;
+            }
+            break;
+
+        case STATE_NUMBER_POINT:
+            if (isdigit(c))
+            {
+                state = STATE_NUMBER_DOUBLE;
+                strAddChar(attr, c);
+            }
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_NUMBER_DOUBLE:
+            if (isdigit(c))
+                strAddChar(attr, c);
+            else if (c == 'e' || c == 'E')
+            {
+                state = STATE_NUMBER_EXPONENT;
+                strAddChar(attr, c);
+            }
+            else
+            {
+                ungetc(c, source);
+                return TYPE_FLOAT;
+            }
+            break;
+
+        case STATE_NUMBER_EXPONENT:
+            if (c == '+' || c == '-')
+            {
+                state = STATE_NUMBER_EXPONENT_SIGN;
+                strAddChar(attr, c);
+            }
+            else if (isdigit(c))
+            {
+                state = STATE_NUMBER_EXPONENT_FINAL;
+                strAddChar(attr, c);
+            }
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_NUMBER_EXPONENT_SIGN:
+            if (isdigit(c))
+            {
+                state = STATE_NUMBER_EXPONENT_FINAL;
+                strAddChar(attr, c);
+            }
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_NUMBER_EXPONENT_FINAL:
+            if (isdigit(c))
+                strAddChar(attr, c);
+            else
+            {
+                ungetc(c, source);
+                return TYPE_FLOAT;
+            }
+            break;
+
+        case STATE_STRING_START:
+            if (c == '"')
+                return TYPE_STRING;
+            else if (c == '\\')
+                state = STATE_STRING_ESCAPE;
+            else if (c < 32)
+                return LEX_ERROR;
+            else if (c == EOF)
+                return LEX_ERROR;
+            else
+                strAddChar(attr, c);
+            break;
+
+        case STATE_STRING_ESCAPE:
+            if (c == 'n')
+            {
+                strAddChar(attr, '\n');
+                state = STATE_STRING_START;
+            }
+            else if (c == 't')
+            {
+                strAddChar(attr, '\t');
+                state = STATE_STRING_START;
+            }
+            else if (c == '"')
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_START;
+            }
+            else if (c == '\\')
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_START;
+            }
+            else if (c == '$')
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_START;
+            }
+            else if (c == 'x')
+            {
+                state = STATE_STRING_ESCAPE_X;
+                strAddChar(attr, c);
+            }
+            else if (c == '0')
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_ESCAPE_ZERO;
+            }
+            else if (c == '1' || c == '2' || c == '3')
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_ESCAPE_ONE_THREE;
+            }
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_STRING_ESCAPE_X:
+            if (c == '0')
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_ESCAPE_X_ZERO;
+            }
+            else if ((isdigit(c) && c != '0') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'))
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_ESCAPE_X_NOT_ZERO;
+            }
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_STRING_ESCAPE_X_ZERO:
+            if ((isdigit(c) && c != '0') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'))
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_START;
+            }
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_STRING_ESCAPE_X_NOT_ZERO:
+            if (isdigit(c) || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'))
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_START;
+            }
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_STRING_ESCAPE_ZERO:
+            if (c == '0')
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_ESCAPE_ZERO_ZERO;
+            }
+            else if (c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7')
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_ESCAPE_REST;
+            }
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_STRING_ESCAPE_ZERO_ZERO:
+            if (c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7')
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_START;
+            }
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_STRING_ESCAPE_REST:
+            if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7')
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_START;
+            }
+            else
+                return LEX_ERROR;
+            break;
+
+        case STATE_STRING_ESCAPE_ONE_THREE:
+            if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7')
+            {
+                strAddChar(attr, c);
+                state = STATE_STRING_ESCAPE_REST;
+            }
+            else
+                return LEX_ERROR;
+            break;
+        }
+    }
+}
---
+++ src/scaner.h
@@ -0,0 +1,97 @@
+// hlavicka pro lexikalni analyzator
+#ifndef SCANNER_H
+#define SCANNER_H
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+// identifikator
+#define ID 0
+
+// klicova slova
+#define ELSE 10
+#define FLOAT 11
+#define FUNCTION 12
+#define IF 13
+#define INT 14
+//#define NULL        15
+#define RETURN 16
+#define STRING 17
+#define VOID 18
+#define WHILE 19
+
+// operatory
+#define PLUS 20       // +
+#define MINUS 21      // -
+#define MUL 22        // *
+#define DIV 23        // /
+#define EQUAL 24      // ===
+#define NOT_EQUAL 25  // !==
+#define LESS 26       // <
+#define GREATER 27    // >
+#define LESS_EQ 28    // <=
+#define GREATER_EQ 29 // >=
+
+// jednotlive znaky
+#define LEFT_PAR 30  // '('
+#define RIGHT_PAR 31 // ')'
+#define SEMICOLON 32 // ';'
+#define COLON 33     // ':'
+#define ASSIGN 34    // '='
+#define COMMA 35     // ','
+#define POINT 36     // '.'
+#define LEFT_BR 37   // '{'
+#define RIGHT_BR 38  // '}'
+#define DO_QUOTES 39 // '"'
+
+#define TYPE_INT 40
+#define TYPE_FLOAT 41
+#define TYPE_STRING 42
+
+// specialni znaky
+#define END_OF_FILE 50
+#define END_OF_LINE 51
+
+// chybove hlasky
+#define LEX_ERROR 1
+
+// Scanner states.
+#define STATE_START 200                  /// Starting state every new token processing starts here and initializes other states
+#define STATE_BACKSLASH 202              /// Operator / OR start of (block) comment - next symbol must be (*) /
+#define STATE_COMMENTARY 201             /// Starts with //, ignores every symbol, ends with EOL or EOF
+#define STATE_BLOCK_COMMENTARY 203       /// Starts with /* and ignores every symbol except * - this might be the end of block comment
+#define STATE_BLOCK_COMMENTARY_LEAVE 204 /// Ends with */ the * is read, if the next symbol is /, leave, else if *, stay, else go back to state before
+#define STATE_IDENTIFIER_OR_KEYWORD 205  /// Starts with letter or _, if next symbols are alphanumeric or _, add them to string, which is later compared with reserved words | Returns either keyword or string as ID
+
+#define STATE_EQUAL_START 206           /// Starts with =, if next symbol is =, return EQUAL, else return ASSIGN
+#define STATE_EQUAL 207                 /// Starts with =, if next symbol is =, return EQUAL, else return ASSIGN
+#define STATE_NOT_EQUAL 208             /// Starts with !, if next symbol is =, return NOT_EQUAL, else return LEX_ERROR
+#define STATE_NOT_EQUAL_START 209       /// Starts with !, if next symbol is =, return NOT_EQUAL, else return LEX_ERROR
+#define STATE_DIV 210                   /// Starts with /, if next symbol is /, return DIV, else return LEX_ERROR
+#define STATE_NUMBER 211                /// Start of number processing, accepts numbers, e/E and . | Can return integer number
+#define STATE_NUMBER_POINT 212          /// If symbol was ., the number has type double
+#define STATE_NUMBER_DOUBLE 213         /// The last symbol was number | Can return double number
+#define STATE_NUMBER_EXPONENT 214       /// The last symbol was e or E, the number has type double, continues with optional symbols +/- or number
+#define STATE_NUMBER_EXPONENT_SIGN 215  /// Optional symbol was read, continue with numbers only
+#define STATE_NUMBER_EXPONENT_FINAL 216 /// Returns double number with exponent
+
+#define STATE_STRING_START 217  /// String starts with !" else returns error
+#define STATE_STRING 218        /// Sequence !" was read, ends with ", if ASCII value is lower than 32, returns error, these symbols can be written using escape sequence | Returns string
+#define STATE_STRING_ESCAPE 219 /// If symbol \ was loaded, can replace char with escape sequence symbols
+#define STATE_STRING_ESCAPE_X 220
+#define STATE_STRING_ESCAPE_X_ZERO 221
+#define STATE_STRING_ESCAPE_X_NOT_ZERO 222
+#define STATE_STRING_ESCAPE_ZERO 223      /// 0 loaded, accepts only digits
+#define STATE_STRING_ESCAPE_ZERO_ZERO 224 /// 00 loaded, accepts only digits
+#define STATE_STRING_ESCAPE_ONE_THREE 225 /// first 2 then 5 was loaded, accepts only 0 to 5 digits, returns symbol with ASCII value
+#define STATE_STRING_ESCAPE_ONE_THREE_REST 226
+#define STATE_STRING_ESCAPE_REST 227 /// the rest cases, when the third number can be only digit, from 001 to 249
+#define STATE_LESS_THAN 228          /// Starts with < | Returns <= or <
+#define STATE_MORE_THAN 229          /// Starts with > | Returns > or >=
+
+// hlavicka funkce simulujici lexikalni analyzator
+void setSourceFile(FILE *f);
+int getNextToken(string *attr);
+
+#endif /* SCANNER_H*/
