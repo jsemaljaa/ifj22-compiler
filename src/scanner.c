69a0dccc7d2ced51e8bd587a74bfa3a0d4546ce6
jsemalja <jsemaljaa@gmail.com>
2022-10-20 10:34:25 +0200

    [project_structure] removed old scanner.[c,h] files

--- src/scanner.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by jsema on 10/6/2022.
-//
-
-#include "scanner.h"
--- src/scanner.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by jsema on 10/6/2022.
-//
-
-#ifndef PROJECT_SCANNER_H
-#define PROJECT_SCANNER_H
-
-#endif //PROJECT_SCANNER_H
